802ab68c137130ebe5975364a347f0dcea1a4f7a
Ed Howard <edward.x.howard@veeam.com>
2021-02-25 15:05:24 +0000

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-session_report
+## Veeam Session Report to xlsx
+
+This program will turn a folder of Veeam session report information (html) into a single excel file.
+
+Gathering session information for VM backups can be done via PowerShell; however, agent backups cannot be so this is the main intention of this program. 
+
+However, there is no reason why it cannot be used VM backups as well. It hasn't been tested on NAS yet.
+
+Packages required:
+
+    pip install BeautifulSoup pandas numpy PySimpleGUI
+
+Session reports can be created via:
+
+![button](/sessio_button.png)
+
+A pop up dialogue box will appear when run with:
+
+    python session_report.py
+
+Select the folder with the reports then press 'ok'.
+
+Note that the program will create a small .pkl file while running which is used to hold the data between each execution. It will
+be deleted automatically after the final run.
+
+Note the program assumes that each run has the same quantity of hosts per-report, per-session. If there are more/less in one or more then
+program may skip that report or the 'name' column maybe skewed.
---
+++ session_report.py
@@ -0,0 +1,118 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import datetime
+import PySimpleGUI as sg
+import os
+import pickle
+
+
+def runJob(file):
+
+    with open(file) as html_file:
+        soup = BeautifulSoup(html_file, 'lxml')
+
+    find_table = soup.find_all('table')
+    rows = find_table[0].find_all('tr')
+
+    results = []
+
+    sessionInfo = []
+
+    session = find_table[0].find_all(class_='sessionDetails')
+
+    for item in session:
+        sessionInfo.append(item.span.text)
+
+    for i in rows:
+        table_data = i.find_all('td')
+        data = [j.text.strip() for j in table_data]
+        results.append(data)
+
+    new_table = pd.DataFrame(results)
+
+    # Strip back to just the required columns
+    new_table = new_table.iloc[:,0:9]
+
+    # Name the columns
+    new_table.columns = ['Name', 'Status', 'Start Time', 'End Time', 'Size', 'Read', 'Transferred', 'Duration', 'Details']
+
+    # Filter out the everything apart without Success or Error
+    new_table = new_table[(new_table['Status'].str.contains('Success')) | (new_table['Status'].str.contains('Error'))]
+
+    # drop the nan 
+    new_table = new_table.dropna()
+
+    # a bunch of splits
+    new_table[['Size', 'Size Metric']] = new_table['Size'].str.split(expand=True)
+    new_table[['Read', 'Read Metric']] = new_table['Read'].str.split(expand=True)
+    new_table[['Transferred', 'Transfer Metric']] = new_table['Transferred'].str.split(expand=True)
+    new_table['Name'] = new_table['Name'].str.split(expand=True)[0]
+    new_table['End Time'] = new_table['End Time'].str.split(expand=True)[0]
+    new_table['Start Time'] = new_table['Start Time'].str.split(expand=True)[0] # just in case
+
+    # change the types
+    new_table['Size'] = new_table['Size'].astype(float)
+    new_table['Read'] = new_table['Read'].astype(float)
+    new_table['Transferred'] = new_table['Transferred'].astype(float)
+    new_table['Start Time'] = pd.to_datetime(new_table['Start Time'])
+    new_table['End Time'] = pd.to_datetime(new_table['End Time'])
+
+    # update the capacities to GB
+    new_table['Size'] = np.where(new_table['Size Metric'] == 'TB', new_table['Size'] * 1024, new_table['Size']) # assumes source will only be TB or GB
+    new_table['Read'] = np.where(new_table['Read Metric'] == 'TB', new_table['Read'] * 1024, new_table['Read'])
+    new_table['Read'] = np.where(new_table['Read Metric'] == 'MB', new_table['Read'] / 1024, new_table['Read']) # just in case
+    new_table['Transferred'] = np.where(new_table['Transfer Metric'] == 'MB', new_table['Transferred'] / 1024, new_table['Transferred'])
+    new_table['Transferred'] = np.where(new_table['Transfer Metric'] == 'TB', new_table['Transferred'] * 1024, new_table['Transferred']) # just in case
+
+    new_table = new_table.drop(['Size Metric', 'Read Metric', 'Transfer Metric'], axis=1)
+
+    mutli = int(len(new_table) / len(sessionInfo))
+
+    new_list = []
+
+    for item in sessionInfo:
+        for _ in range(mutli):
+            new_list.append(item)
+
+    new_table['Date'] = new_list
+    
+    global_df = pd.read_pickle('./data.pkl')
+
+    frames = [global_df, new_table]
+
+    global_df = pd.concat(frames)
+
+    global_df.to_pickle('./data.pkl')
+
+
+if __name__ == '__main__':
+    global_df = pd.DataFrame(columns=['Date','Name', 'Status', 'Start Time', 'End Time', 'Size', 'Read', 'Transferred', 'Duration', 'Details'])
+
+    # delete pickle file if it still exists from a previous run
+    if os.path.exists('data.pkl'):
+        os.remove('data.pkl')
+    
+    global_df.to_pickle('./data.pkl')
+    
+    directory = sg.popup_get_folder('Please select folder')
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".html"):
+            try:
+                runJob(filename)
+            except:
+                print(f"file {filename} did not work")
+
+    global_df2 = pd.read_pickle('./data.pkl')
+
+    global_df2.reset_index(drop=True, inplace=True)
+
+    now_time = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+
+    global_df2.to_excel(f"results-{now_time}.xlsx")
+
+    if os.path.exists("data.pkl"):
+        os.remove('data.pkl')
+    else:
+        print('No pickle to delete!')
